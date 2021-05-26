e8341c66f10a595b5bbaa2502f337ca3b534fb93
Sebastian <sebastian@starck.im>
2021-05-26 00:22:45 -0300

    Tidied up dependencies

--- src/services/RequestService.ts
+++ src/services/RequestService.ts
@@ -3,19 +3,18 @@ import { DDdragonAssetCollection } from "./types";
 export class RequestService {
   public static async get<T>(url: string): Promise<T> {
     const response = await fetch(url);
-
     if (!response.ok) {
       throw new Error("REQUEST_FAILED");
     }
 
     return response.json().then((data) => data);
   }
-
-  public static getDDragonAssetCollection<T>(
-    collection: DDdragonAssetCollection
-  ): Promise<T[]> {
-    const url = `http://ddragon.leagueoflegends.com/cdn/11.10.1/data/en_US/${collection}.json`;
-
-    return RequestService.get<T[]>(url);
-  }
+  //
+  // public static getDDragonAssetCollection<T>(
+  //   collection: DDdragonAssetCollection
+  // ): Promise<T[]> {
+  //   const url = `http://ddragon.leagueoflegends.com/cdn/11.10.1/data/en_US/${collection}.json`;
+  //
+  //   return RequestService.get<T[]>(url);
+  // }
 }
